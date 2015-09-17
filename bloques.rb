42bc78765fb72be2a5e9c769704984305c1e96ff
diegozwolf <diegozuluaga88@gmail.com>
2015-09-16 20:00:55 -0500

    se vió comparacion de variables y retorno de mensajessegun el resultado true o false

---
+++ bloques.rb
@@ -0,0 +1,23 @@
+=begin
+creo un bloque donde se mostrara un mensaje si dos valores
+son iguales
+=end
+
+
+if "diego" == "diego" #se uso 2 signos igual(=)¿Por qué? un signoigual asignará un valor
+
+	puts "diego es igual a Andres" #dos iguales (==) comparara un valor
+end
+
+#Provocando un erros
+if "diego" == "Fuckencio"
+	puts "diego es igual a Fuckencio"
+end
+
+# rescatando el valor que comparamos
+if "diego" == "hijuemadre"
+	puts "diego es igual a hijuemadre"
+else
+	puts "diego No es igual a hijuemadre"
+end
+
