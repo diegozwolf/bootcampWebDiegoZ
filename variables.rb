5498a6b55104c31944f30eaec501a399d18324ab
diegozwolf <diegozuluaga88@gmail.com>
2015-09-16 19:14:30 -0500

    Repaso de variables y tipos de datos

---
+++ variables.rb
@@ -0,0 +1,26 @@
+#Defini una variable con cadena de texto
+tipoDeMascota = "Perros"
+#Defini una variable con cadena de texto
+tipoDeMascota2 = "Gatos"
+#Defini una variable con un boleano
+tienePerro = true
+#Defini una variable con dato númerico
+numeroDeGatos = 23
+#Defini una variable con dato númerico
+numeroDePerros = 45
+#Defini una variable con aoperacion de variables
+numeroDeanimales = numeroDePerros + numeroDeGatos
+ 
+=begin
+	
+se pone puts para mostrar variables	
+=end
+
+ puts tipoDeMascota
+puts tipoDeMascota2
+puts tienePerro
+puts numeroDeGatos
+puts numeroDePerros
+puts "********"
+puts numeroDeanimales
+
