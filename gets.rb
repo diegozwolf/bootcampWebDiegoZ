0e4643d0b78914922ce05f3bc2adf1194d0c0750
diegozwolf <diegozuluaga88@gmail.com>
2015-09-16 20:02:19 -0500

    uso de gets,chomp,print y concatenación.

---
+++ gets.rb
@@ -0,0 +1,13 @@
+#pido que me dé su primer nombre.
+print "Escriba su primer nombre:"
+#tomo el primer nombre y lo asigno a una variable
+nombre = gets.chomp #agrego .chomp que elimina salto de linea
+#pido el segundo nombre o apellido
+print "escribe tu apellido"
+#guardo el segindo nombre en una variable
+nombre2 = gets.chomp
+#imprimó en la terminal un texto de bienvenida.
+puts "Hola, " + nombre + "" + nombre2 + "."
+# para ejecutar en la terminal se escribe: ruby + nommbredearchi.rb
+#en la terminal se debe terminal ruby (irb y darle quit) 
+
