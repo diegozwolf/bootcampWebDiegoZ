68e23b0f160be30a5acda7f1999cad0b775f7455
diegozwolf <diegozuluaga88@gmail.com>
2015-09-17 19:44:53 -0500

    ejercicioo de corrección de errores en un script.

---
+++ ejercicio3.rb
@@ -0,0 +1,14 @@
+nombre = "Diego"
+nombre = "diego"
+edad = 23
+ciudad = "Bogotá"
+#Bogota es igual a bogota
+#Debería ejecutar el puts
+if ciudad == "Bogotá" then
+	puts "la ciudad es Bogotá"
+end
+#el arreglo debería tener 5 elementos
+arreglo =  ["Manzana", "Limon", 3, 2.4, "Pera"]
+
+#Quiero imprimir el últimoelemento del arreglo.
+puts arreglo[4]
