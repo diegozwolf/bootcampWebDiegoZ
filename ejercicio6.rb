ca029f1f10897cda8cc53c9de513a8123180c510
diegozwolf <diegozuluaga88@gmail.com>
2015-09-18 19:34:46 -0500

    metodos y ejercicios en ruby

---
+++ ejercicio6.rb
@@ -0,0 +1,15 @@
+# Usando gets pide el nombre y el apellido del usuario y luego
+#imprime los datos en terminal.
+
+puts "Escribe tu nombre "
+nombre = gets.chomp
+
+puts "Escribe tu apellido " # Al usar prints el textos capturadfo se escribe en la misma linea
+apellido = gets.chomp
+
+	#puts "El nombre es: #{nombre} #{apellido}"
+def saludo (nombre, apellido)
+	puts "Hola: #{nombre} #{apellido}"
+end
+
+saludo (nombre apellido)
---
+++ metodos.rb
@@ -0,0 +1,15 @@
+def saludar
+	puts "Que onda mi guero!"
+	if "Diego" == "Diego"
+		puts "Diego SÍ es iagual a Diego"
+	else
+		puts "Diego No es igual a Diego"
+	end	
+end
+
+saludar
+def hablar(texto)
+	puts "El metodo dice#{texto}"
+end
+
+hablar(" Hola estoy hablando con un método que recibe argumentos.")
